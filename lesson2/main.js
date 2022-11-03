db7f77bec1eba6b9658ab5f06f941725525dd587
Admin <sorokivskyi777@gmail.com>
2022-11-03 11:03:17 +0200

    have done the lesson2 hw

---
+++ lesson2/main.js
@@ -0,0 +1,186 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let array = [];
+// array[0] = 'vasya';
+// array[array.length] = 11;
+// array[array.length] = true;
+// array[array.length] = 'Misha';
+// array[array.length] = 'Danik';
+// array[array.length] = 33;
+// array[array.length] = 'Michael';
+// array[array.length] = false;
+// array[array.length] = 'Nana';
+// array[array.length] = 'jsdfkljdsf';
+// array[array.length] = 222;
+// array[array.length] = 43444444324;
+// console.log(array);
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+// console.log(array[(array.length -1)]);
+
+
+
+//
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let book1 = {
+//     title: 'Food',
+//     pageCount: 33,
+//     genre: 'cooking'
+// };
+//
+// let book2 = {
+//     title: 'The Witcher',
+//     pageCount: 333,
+//     genre: 'adventures'
+// };
+//
+// let book3 = {
+//     title: 'The FBI files',
+//     pageCount: 200,
+//     genre: 'detective'
+// };
+//
+// let book4 = {
+//     title: 'Fishing',
+//     pageCount: 100,
+//     genre: 'nature'
+// };
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let book5 = {
+//     title: 'American Dream',
+//     pageCount: 220,
+//     genre: 'development',
+//     authors: [
+//         {
+//             name: 'Rodger',
+//             age: 55
+//         },
+//         {
+//             name: 'Sam',
+//             age: 44
+//         },
+//         {
+//             name: 'Lincoln',
+//             age: 56
+//         }
+//     ]
+// };
+//
+// let book6 = {
+//     title: 'Perfect Coding',
+//     pageCount: 444,
+//     genre: 'coding',
+//     authors: [
+//         {
+//             name: 'Jim',
+//             age: 45
+//         },
+//         {
+//             name: 'Marty',
+//             age: 33
+//         },
+//         {
+//             name: 'Dorothy',
+//             age: 34
+//         }
+//     ]
+// };
+//
+// let book7 = {
+//     title: 'Scoobee Do',
+//     pageCount: 567,
+//     genre: 'fun',
+//     authors: [
+//         {
+//             name: 'Jericko',
+//             age: 22
+//         },
+//         {
+//             name: 'Kerry',
+//             age: 45
+//         },
+//         {
+//             name: 'Aniston',
+//             age: 34
+//         }
+//     ]
+// };
+
+
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// let arr = [
+//     {
+//         name: 'Tigy',
+//         username: 'black',
+//         password: 12344489490
+//     },
+//     {
+//         name: 'Johnny',
+//         username: 'John',
+//         password: 4343580439750893
+//     },
+//     {
+//         name: 'Drag',
+//         username: 'drag',
+//         password: 858942345892
+//     },
+//     {
+//         name: 'Vsevolod',
+//         username: 'vsevolod',
+//         password: 358903475
+//     },
+//     {
+//         name: 'Vasya',
+//         username: 'vasya',
+//         password: 3487598
+//     },
+//     {
+//         name: 'Vicka',
+//         username: 'viky',
+//         password: 7348957
+//     },
+//     {
+//         name: 'Dodo',
+//         username: 'johnDoe',
+//         password: 'shd433oi4'
+//     },
+//     {
+//         name: 'Karina',
+//         username: 'Ka',
+//         password: 'HJo39hjfd'
+//     },
+//     {
+//         name: 'Din',
+//         username: 'dih',
+//         password: 'FDHFJKJDFHJH88839'
+//     },
+//     {
+//         name: 'Dom',
+//         username: '',
+//         password: '7234897fhfff'
+//     }
+// ];
+//
+// console.log(arr[0].password);
+// console.log(arr[1].password);
+// console.log(arr[2]['password']);
+// console.log(arr[3]['password']);
+// console.log(arr[4]['password']);
+// console.log(arr[5].password);
+// console.log(arr[6].password);
+// console.log(arr[7].password);
+// console.log(arr[8].password);
+// console.log(arr[(arr.length - 1)].password);
