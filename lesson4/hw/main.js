94904f629cff1c3f0880d31d7ef7a744e8549283
Admin <sorokivskyi777@gmail.com>
2022-11-10 19:53:52 +0200

    have done the lesson4 hw tasks (adjust)

--- lesson4/hw/main.js
+++ lesson4/hw/main.js
@@ -46,12 +46,12 @@
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 //
 // let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write(`<ul>`);
 // for (let i = 0; i < listOfItems.length; i++) {
-//     document.write(`<ul>
-//     <li>${listOfItems[i]} </li>
-// </ul>`)
+//     document.write(`
+//     <li>${listOfItems[i]} </li>`)
 // }
-
+// document.write(`<ul>`);
 
 // -----------------------------------------------
 //
