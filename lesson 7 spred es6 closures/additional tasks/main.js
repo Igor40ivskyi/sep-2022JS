2db1b2597d63600884d25e497c6da01bd33ff2e5
Admin <sorokivskyi777@gmail.com>
2022-12-01 15:52:24 +0200

    lesson 7 trying to do the additional task

---
+++ lesson 7 spred es6 closures/additional tasks/main.js
@@ -0,0 +1,61 @@
+let userCard = (number) => {
+    let userCardObj =
+        {
+            balance:100,
+            transactionLimit:100,
+            historyLogs: [],
+            key: 1,
+        }
+
+    let card1 = {
+        balance: 0,
+        transactionLimit: 100,
+        historyLogs: [],
+        key: 2,
+    };
+
+        return {
+            getCardOptions: function () {
+                return {...userCardObj};
+            },
+            putCredits: function (amountOfMoney) {
+                userCardObj.balance = userCardObj.balance + amountOfMoney;
+            },
+            takeCredits: function (amountOfMoney) {
+                if (userCardObj.transactionLimit > amountOfMoney && userCardObj.balance > amountOfMoney) {
+
+                userCardObj.balance = userCardObj.balance - amountOfMoney;
+                }else {
+                    throw new Error('too much!');
+                }
+            },
+            setTransactionLimit: function (number) {
+                userCardObj.transactionLimit = number;
+            },
+            transferCredit: function (amountOfMoney, cardOfUser) {
+                cardOfUser.balance = cardOfUser.balance + amountOfMoney - (amountOfMoney / 100) * 0.5;
+            },
+            getcard1Options: function () {
+                return {...card1};
+            },
+        }
+};
+
+let card1 = userCard(3);
+
+console.log(card1.getCardOptions());
+card1.putCredits(1000);
+console.log(card1.getCardOptions());
+
+card1.takeCredits(99);
+console.log(card1.getCardOptions());
+card1.setTransactionLimit(1000);
+card1.takeCredits(500);
+
+console.log(card1.getCardOptions());
+
+card1.transferCredit(1000,card1);
+console.log(card1.getcard1Options());
+
+card1.transferCredit(1000, card1);
+console.log(card1.getcard1Options());
