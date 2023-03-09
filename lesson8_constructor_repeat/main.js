7b0969365e85b74ffc137d3041b1688be97f08b4
Admin <sorokivskyi777@gmail.com>
2023-03-09 16:07:26 +0200

    lesson 8 training

--- lesson8_constructor_repeat/main.js
+++ lesson8_constructor_repeat/main.js
@@ -102,32 +102,119 @@
 
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 
-class Client {
+// class Client {
+//
+//     constructor(id, name, surname, email, phone, order) {
+//         this.id = id;
+//         this.name = name;
+//         this.surname = surname;
+//         this.email = email;
+//         this.phone = phone;
+//         this.order = order;
+//     }
+//
+// }
+//
+// let client = new Client(4, 'dima', 'dlfjdf', 'sdfjkl@gmail', 324324, [24, 234, 234]);
+//
+// console.log(client);
 
-    constructor(id, name, surname, email, phone, order) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
-        this.order = order;
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car,
+// з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+// function Car(model, brand, year, maxSpeed, volume) {
+//     this.model = model;
+//     this.brand = brand;
+//     this.year = year;
+//     this.maxSpeed = maxSpeed;
+//     this.volume = volume;
+//     this.drive = function () {
+//         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+//     };
+//     this.info = function () {
+//         for (let key in this) {
+//            if (typeof this[key] !== "function") console.log(`${key} - ${this[key]}`)
+//         }
+//     };
+//     this.increaseMaxSpeed = function (newSpeed) {
+//         this.maxSpeed += newSpeed;
+//     };
+//
+//     this.changeYear = function (newValue) {
+//         this.year = newValue;
+//     };
+//     this.addDriver = function (driver) {
+//         this.driver = driver;
+//     };
+// }
+//
+// let car = new Car('s5', 'bmw', 2000, 300, 3.5);
+//
+// console.log(car);
+//
+// car.info();
+//
+// car.increaseMaxSpeed(40);
+//
+// console.log(car.maxSpeed);
+
+// Створити клас який дозволяє створювати об'єкти car, з властивостями
+// модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+class Car {
+    model;
+    brand;
+    year;
+    maxSpeed;
+    volume;
+
+    constructor(model, brand, year, maxSpeed, volume) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.volume = volume;
     }
 
+    drive() {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} per hour`)
+    }
+
+    info() {
+        for (let key in this) {
+            if (typeof this[key] !== "function") {
+                console.log(`${key} - ${this[key]}`);
+            }
+        }
+    }
+
+    increaseMaxSpeed(newSpeed) {
+        this.maxSpeed += newSpeed
+    }
+
+    changeYear(newYear) {
+        this.year = newYear;
+    }
+
+    addDriver() {
+
+    }
 }
 
-let client = new Client(4, 'dima', 'dlfjdf', 'sdfjkl@gmail', 324324, [24, 234, 234]);
+let car = new Car('mers', 'kadilak', 3000, 300, 34);
 
-console.log(client);f
-
-
-
-
-
-
-
-
-
-
+car.info()
 
 
 
